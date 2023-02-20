cfadd9b6c783e3e6f97b0d481088d4585eccb852
Dmitriy <101889433+akiko23@users.noreply.github.com>
2023-02-20 13:59:47 +0300

    Update task6.py

--- task6.py
+++ task6.py
@@ -3,13 +3,18 @@ a, b, c = int(input()), int(input()), int(input())
 # вычисление дискриминанта
 discriminant = b**2 - 4 * a * c
 
-# вычисление 1 и 2 корней по известным всем формулам
-x1 = int(-b - discriminant**0.5 / (2 * a))
-x2 = int(-b + discriminant**0.5 // (2 * a))
+# проверка на отрицательный дискриминант
+if discriminant < 0:
+    print("Нет решений")
 
-# проверка на совпадение корней
-if x1 == x2:
-    print(x1)
 else:
-    print(x1)
-    print(x2)
+    # вычисление 1 и 2 корней по известным всем формулам
+    x1 = int(-b - discriminant**0.5 / (2 * a))
+    x2 = int(-b + discriminant**0.5 // (2 * a))
+
+    # проверка на совпадение корней
+    if x1 == x2:
+        print(x1)
+    else:
+        print(x1)
+        print(x2)
